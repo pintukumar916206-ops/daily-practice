f74dff3f1e8452509fe0eb8acfbb8099135fd62e
Pintu Kumar <pintukumar916206@gmail.com>
2026-03-07 23:45:24 +0530

    Daily Update

--- 07.UI-project/src/components/section1/Hero.jsx
+++ 07.UI-project/src/components/section1/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
         <p className="text-xl w-[90%] font-medium text-gray-600">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit.
           Ullam libero ad distinctio voluptatem! Iure enim dignissimos repudiandae ea, 
-          officiis eius? Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          officiis eius? 
         </p>
       </div>
     </div>
@@ -20,3 +20,4 @@ const Hero = () => {
 }
 
 export default Hero
+
