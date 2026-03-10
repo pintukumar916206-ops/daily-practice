fd8e1af652fd4d17155332a2e35f371ed7340ff2
Pintu Kumar <pintukumar916206@gmail.com>
2026-03-10 23:34:08 +0530

    Daily Update

--- 07.UI-project/src/components/section1/Hero.jsx
+++ 07.UI-project/src/components/section1/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
         <p className="text-xl w-[90%] font-medium text-gray-600">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit.
           Ullam libero ad distinctio voluptatem! Iure enim dignissimos repudiandae ea, 
-          officiis eius? 
+          officiis eius? lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
         </p>
       </div>
     </div>
