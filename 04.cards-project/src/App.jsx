abfdd51a28ea2335b3c51965fcb0879c403c75f3
Pintu Kumar <pintukumar916206@gmail.com>
2026-03-17 10:37:38 +0530

    Daily Update

--- 04.cards-project/src/App.jsx
+++ 04.cards-project/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       tag2: "Junior Level",
       pay: 45,
       location: "Patna, India",
-      applicant: "Naruto Uzumaki(Naruto Namikaze)"
+      applicant: "Naruto Uzumaki"
     },
     {
       brandLogo: "https://images.unsplash.com/photo-1664464168934-637a185dea7e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
